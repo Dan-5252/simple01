0ae9dfe51755dd47f6495cc602b7b170c7445918
Dan-5252 <Dan-5252@users.noreply.github.com>
2022-10-21 22:10:37 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+function l(x) {
+  console.log(x);
+}
+l("hi");
+const doer = () => 5;
+l(doer());
+
+class rr {
+  constructor(x) {
+    this.v = x;
+  }
+  write() {
+    l(this.v);
+  }
+  forYou(x) {
+    return x("banana");
+  }
+}
+
+const cheese = new rr("toy");
+
+l(cheese.v);
+cheese.write();
+cheese.forYou(l);
